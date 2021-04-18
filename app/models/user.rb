0eab235a8dc152ba3b6f7bd23d48e6053549d297
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-18 11:02:13 +0900

    ユーザー管理機能　コメントアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,18 +24,3 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 end
-#last_nameとfirst_nameが漢字とひらがなであることを確認する正規表現が必要
-#last_name_kanaとfirst_name_kanaがカタカナであることを確認する正規表現が必要
-
-#presence: trueをまとめる
-
-# with_options presence: true, do
-#   validates :name
-#   with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }, do
-#   validates :last_name
-#   validates :first_name
-#   end
-#   validates :last_name_kana, presence: true
-#   validates :first_name_kana, presence: true
-#   validates :birth_date, presence: true
-# end
