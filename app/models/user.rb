b929feffe32864025600df491340c49369cf3f3f
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-18 10:55:08 +0900

    ユーザー管理機能修正1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
-  validates :last_name, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' }
-  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' }
-  validates :birth_date, presence: true
-  validates :password,presence: true, length: {minimum: 6 }
+  with_options presence: true do
+    validates :name,
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name
+    validates :first_name 
+  end
+  validates :name
+  with_options presence: true do
+  validates :last_name_kana format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' }
+  end
+  validates :birth_date
+  validates :password length: {minimum: 6 }
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates :password, presence: true,
             format: { with: VALID_PASSWORD_REGEX,
