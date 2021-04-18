4e94c70af59ff632f5de7493a57b8645414706a5
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-18 12:21:48 +0900

    ユーザー管理機能　修正3回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,2 @@
 class ItemsController < ApplicationController
-  def index
-    @item = Item.new
-  end
-  
-  def create
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context 'ユーザー新規登録ができる時' do
     it '全項目が入力されていれば登録できる' do
       expect(@user).to be_valid
     end
-    
+  end
+    context 'ユーザー新規登録ができない時' do
     it 'nameが空では登録できない' do
       @user.name = ''
       @user.valid?
@@ -43,11 +45,7 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
-    it 'メールアドレスは、@を含む必要があること' do
-      @user.email = '@aaaa'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
-    end
+  
     it 'パスワードは、6文字以上での入力が必須であること（6文字が入力されていれば、登録が可能なこと）'do
       @user.password = '12345'
       @user.valid?
@@ -127,6 +125,7 @@ RSpec.describe User, type: :model do
       @user.password = '1234'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password は半角6文字以上の半角英数字")
+    end
     end
   end
 end
