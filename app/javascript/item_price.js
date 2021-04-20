7c76c19af404b0fd20a1e35ffc2a269ae24bbe44
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-20 11:43:18 +0900

    商品出品機能2 販売価格

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue)
+    
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+    const addTaxPro   = document.getElementById("profit")
+    addTaxPro.innerHTML = Math.floor(inputValue * 0.9)
+    console.log(addTaxPro)
   }) 
   })
 
