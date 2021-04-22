0329e9042a5d2b1d248d29857d6f699543139d30
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-22 16:22:30 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
-
-    
-
-     @items = Item.order("created_at DESC")
-
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -27,15 +23,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def destroy
-  # end
+   def destroy
+        @item = Item.find(params[:id])
+     if  @item.destroy
+        current_user != @item.user.id
+        redirect_to root_path
+     end
+    end
+   
 
-  # def edit
-  # end
+   def edit
+   end
+   
+   private
 
   def item_params
     params.require(:item).permit(:name, :image, :text, :category_id, :info_id, :shipping_fee_status_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
-
-  
 end
