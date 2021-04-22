d2a0c554dcab985ad249250c598c2e73bafb4790
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-22 11:50:03 +0900

    商品情報編集機能　1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
+
   def index
-
-    
-
-     @items = Item.order("created_at DESC")
-
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -24,25 +22,42 @@ class ItemsController < ApplicationController
   end
   
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def updated
-  end
+  def update
+     @item = Item.find(params[:id])
+     # @item.update(item_params)
+   if @item.valid?
+      @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end 
   
-
   def show
     @item = Item.find(params[:id])
   end
 
-  # def destroy
-  # end
-
-  # def edit
-  # end
-
+  
   def item_params
     params.require(:item).permit(:name, :image, :text, :category_id, :info_id, :shipping_fee_status_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
-
+    
+  def move_to_index
+    unless current_user.id == @item.user.id
+           redirect_to action: :index
+    end
   
-end
+     unless current_user.id?
+            redirect_to action: :index
+    end  
+
+     
+  end         
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+ end
